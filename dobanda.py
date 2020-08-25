bd36d733dc9507d43deb8ad7069ee036723e0460
Bgd <fbogdan21@gmail.com>
2020-08-25 04:21:47 +0300

    Add files via upload

---
+++ dobanda.py
@@ -0,0 +1,120 @@
+# Calcularea Dobanda 22.03.20
+
+a = int(input("Introuceti suma depusa: "))
+#print(a)
+b = float(input("Introduceti dobanda: "))
+#print(b)
+
+win1 = a*b
+
+print("Primul castig pe un an: ",win1)
+print("Castig total primul an: ",win1+a)
+win2 = b*(win1+a)
+print("Al doilea castig: ",win2)
+print("Castig ",win2+win1+a)
+win3 = b*(win2+win1+a)
+print("Al treilea an castig: ",win3)
+print("Total dupa 3 ani",win3+win2+win1+a)
+
+'''
+abuză
+acuză
+amuză
+anacruză
+andropauză
+archebuză
+autoacuza
+bluză
+buburuză
+buză
+cambuză
+cauză
+ceruză
+clauză
+dezabuza
+diapauză
+difuza
+dizeuză
+druză
+duză
+ecluză
+ecluza
+friteuză
+gafeuză
+infuza
+ipotenuză
+lăhuză
+lăuză
+lehuză
+lizeuză
+machieuză
+magnetopauză
+maseuză
+meduză
+menopauză
+mezopauză
+muză
+pantahuză
+pauză
+peluză
+premenopauză
+radiodifuza
+recuza
+refuza
+remaieuză
+repauza
+scuză
+spuză
+taluza
+teledifuza
+termopauză
+transfuza
+tropopauză
+uza
+ventuză
+andaluză
+asambleuză
+buza
+călăuză
+desabuza
+fărtăuză
+găgăuză
+ghiuză
+mauză
+păpăruză
+păuza
+pentahuză
+perfuza
+răzbuza
+soteuză
+statopauză
+stratopauză
+yakuză
+țeruză
+aeropauză
+honoris cauza
+mezuză
+partuză
+preandropauză
+magmuză
+păuză
+farfuză
+pandacuză
+chiuza
+zuză
+cuza
+buburuza
+enfiteuză
+meduza
+fuză
+Sobhuza
+stripteuză
+țăruză
+uză
+Meduza
+Meuza
+Peruza
+Răguza
+Sarmisegetuza
+Siracuza
+Suza
